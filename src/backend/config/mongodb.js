20c2ed31139f7479712e6fc9a63c17e0d0c5e0e9
Erkz <Erkiparna7@gmail.com>
2023-10-26 22:29:40 +0300

    [ Dependencies ] Removed mongodb.js, uninstalled mongoose package

--- src/backend/config/mongodb.js
+++
@@ -1,8 +0,0 @@
-// We are using a secondary database for session storage due to XATA not having a datastore module
-
-const mongoose= require('mongoose');
-
-mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB Connected'))
-    .catch((err) => console.error(err));
